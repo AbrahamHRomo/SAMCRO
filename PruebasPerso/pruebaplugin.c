c83b1a22e8f9da3593fcccf78e3ab1cb320ad6c6
AbrahamHRomo <conde_de_homero@hotmail.com>
2020-10-23 17:21:38 -0500

    alfabeto en prueba perso

---
+++ PruebasPerso/pruebaplugin.c
@@ -0,0 +1,15 @@
+#include<stdlio.h>
+
+int main()
+{
+  char inciso_abecedario '@';
+  int contador;
+printf("\n");
+  for(contador = 0; contador < 28; contador ++)
+    {
+      printf("%c, ", inciso_abecedario + contador);
+    }
+printf("\n");
+  printf("%c", 64);
+
+return 0;}
