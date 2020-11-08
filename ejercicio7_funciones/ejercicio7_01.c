013773fa66b80e2da22a709a7d342a5a4e85b3ce
AbrahamHRomo <conde_de_homero@hotmail.com>
2020-11-08 17:15:31 -0600

    fuck yeahhhhgit add ejercicio7_funciones/!

--- ejercicio7_funciones/ejercicio7_01.c
+++ ejercicio7_funciones/ejercicio7_01.c
@@ -1,17 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-void funcion_ImprimeCuadrado (int *, int *);
+int funcion_ImprimeCuadrilatero (int *, int *);
 
-void main (void){
-        int largo = 10, alto = 5;
-        funcion_ImprimeCuadrado (&largo, &alto);
-        largo += largo;
-        alto += alto;
-        funcion_ImprimeCuadrado (&largo, &alto);
+int main (int no_d_arg, char **valores){
+        int largo, alto;
+        largo = atoi(valores[1]);
+        alto = atoi(valores[2]);
+        funcion_ImprimeCuadrilatero (&largo, &alto);
 }
 
-void funcion_ImprimeCuadrado (int * largo, int * alto){
+int funcion_ImprimeCuadrilatero (int * largo, int * alto){
   char vertical = '|', horizontal = '-', esquina = '+';
 
   int alto_contador, contador_horizontal;
